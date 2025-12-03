7844dd8dd7e93ec2b1b80001fadc43752dd88a8d
Yibo Zhao <yibozhao2297@gmail.com>
2025-12-03 15:02:04 +0000

    add tbi

--- merge_plender_data.sh
+++ merge_plender_data.sh
@@ -2,3 +2,4 @@
 module load bcftools/1.16-kjo5veq  
 cd /data/gen1/yz735/SV_explore/GRCh38 
 bcftools concat -a -Oz -o variants_GRCh38_merged_HGSVC.vcf.gz *vcf.gz  
+bcftools index --tbi variants_GRCh38_merged_HGSVC.vcf.gz
