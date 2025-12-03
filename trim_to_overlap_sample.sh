41de725d82b79c71df43b509f9596d81c4573582
Yibo Zhao <yibozhao2297@gmail.com>
2025-12-03 15:11:27 +0000

    add tbi

--- trim_to_overlap_sample.sh
+++ trim_to_overlap_sample.sh
@@ -2,3 +2,5 @@
 module load bcftools/1.16-kjo5veq  
 bcftools view -S overlap_samples.txt -Oz -o variants_GRCh38_merged_trimmed_HGSVC.vcf.gz variants_GRCh38_merged_HGSVC.vcf.gz  
 bcftools view -S overlap_samples.txt -Oz -o chr11_1kgp_imputed_vep_trimmed.vcf.gz chr11_1kgp_imputed_vep.vcf.gz  
+bcftools index -t variants_GRCh38_merged_trimmed_HGSVC.vcf.gz  
+bcftools index -t chr11_1kgp_imputed_vep_trimmed.vcf.gz  
