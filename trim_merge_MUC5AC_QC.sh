0f8e330e8aab89f6059665501012ce68210d31e1
Yibo Zhao <yibozhao2297@gmail.com>
2025-12-03 15:25:36 +0000

    Update trim_merge_MUC5AC_QC.sh

--- trim_merge_MUC5AC_QC.sh
+++ trim_merge_MUC5AC_QC.sh
@@ -5,3 +5,4 @@ bcftools index merged_MUC_sorted_dedup.vcf.gz
 bcftools view -r chr11:1157593-1201138 merged_MUC_sorted_dedup.vcf.gz -Oz -o merged_MUC_sorted_dedup_MUC5AC.vcf.gz
 bcftools index merged_MUC_sorted_dedup_MUC5AC.vcf.gz
 bcftools stats merged_MUC_sorted_dedup_MUC5AC.vcf.gz > merged_MUC_sorted_dedup_MUC5AC_stats.txt
+## 2033 variants for 63 samples
