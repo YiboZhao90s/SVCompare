0a0785aec02dbfbc4a62044469c9117f80d75bf6
Yibo Zhao <yibozhao2297@gmail.com>
2025-12-03 15:22:37 +0000

    Create trim_merge_MUC5AC_QC.sh

---
+++ trim_merge_MUC5AC_QC.sh
@@ -0,0 +1,7 @@
+## trim the merged MUC file to MUC5AC and QC
+bcftools sort -Oz -o merged_MUC_sorted.vcf.gz merged_MUC.vcf.gz
+bcftools norm -d both -Oz -o merged_MUC_sorted_dedup.vcf.gz merged_MUC_sorted.vcf.gz
+bcftools index merged_MUC_sorted_dedup.vcf.gz
+bcftools view -r chr11:1157593-1201138 merged_MUC_sorted_dedup.vcf.gz -Oz -o merged_MUC_sorted_dedup_MUC5AC.vcf.gz
+bcftools index merged_MUC_sorted_dedup_MUC5AC.vcf.gz
+bcftools stats merged_MUC_sorted_dedup_MUC5AC.vcf.gz > merged_MUC_sorted_dedup_MUC5AC_stats.txt
