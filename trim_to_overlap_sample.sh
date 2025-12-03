422d3c150efc5523f9935ba8a7daac72c463649a
Yibo Zhao <yibozhao2297@gmail.com>
2025-12-03 15:09:02 +0000

    trim webster and plender data based on overlapped samples

---
+++ trim_to_overlap_sample.sh
@@ -0,0 +1,4 @@
+# Trim Lucy_Data and Plender_Data for overlapped samples
+module load bcftools/1.16-kjo5veq  
+bcftools view -S overlap_samples.txt -Oz -o variants_GRCh38_merged_trimmed_HGSVC.vcf.gz variants_GRCh38_merged_HGSVC.vcf.gz  
+bcftools view -S overlap_samples.txt -Oz -o chr11_1kgp_imputed_vep_trimmed.vcf.gz chr11_1kgp_imputed_vep.vcf.gz  
