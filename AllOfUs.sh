6612640bcf9076c18a3c6f522d8faa9cc7846fa6
Yibo Zhao <yibozhao2297@gmail.com>
2025-12-10 13:53:32 +0000

    Update AllOfUs.sh

--- AllOfUs.sh
+++ AllOfUs.sh
@@ -1,4 +1,2 @@
 # Note for All of Us workbench
-# long read VCF location
-# gs://fc-aou-datasets-controlled/v8/wgs/long_read/BCM/ont/joint_call/GRCh38/v8.BCM_ONT_high.QualFT34.g.vcf.*
-gsutil -u terra-vpc-sc-7210ba51 cp gs://fc-aou-datasets-controlled/v8/wgs/long_read/BCM/ont/joint_call/GRCh38/v8.BCM_ONT_high.QualFT34.g.vcf* ~/workspaces/muc5acsv/
+
