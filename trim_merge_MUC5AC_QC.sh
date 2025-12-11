7b5268309d97eb92afc1375c93329a3874d5182a
Yibo Zhao <yibozhao2297@gmail.com>
2025-12-11 15:28:20 +0000

    expand the region to cover +- 20kb and include MUC5AC and MUC5B

--- trim_merge_MUC5AC_QC.sh
+++ trim_merge_MUC5AC_QC.sh
@@ -1,8 +1,9 @@
 ## trim the merged MUC file to MUC5AC and QC
+module load bcftools/1.16-kjo5veq
 bcftools sort -Oz -o merged_MUC_sorted.vcf.gz merged_MUC.vcf.gz
 bcftools norm -d both -Oz -o merged_MUC_sorted_dedup.vcf.gz merged_MUC_sorted.vcf.gz
 bcftools index merged_MUC_sorted_dedup.vcf.gz
-bcftools view -r chr11:1157593-1201138 merged_MUC_sorted_dedup.vcf.gz -Oz -o merged_MUC_sorted_dedup_MUC5AC.vcf.gz
-bcftools index merged_MUC_sorted_dedup_MUC5AC.vcf.gz
-bcftools stats merged_MUC_sorted_dedup_MUC5AC.vcf.gz > merged_MUC_sorted_dedup_MUC5AC_stats.txt
-## 2033 variants for 63 samples
+bcftools view -r chr11:1137953-1282172 merged_MUC_sorted_dedup.vcf.gz -Oz -o merged_MUC_sorted_dedup_MUC5AC_MUC5B.vcf.gz
+bcftools index merged_MUC_sorted_dedup_MUC5AC_MUC5B.vcf.gz
+bcftools stats merged_MUC_sorted_dedup_MUC5AC_MUC5B.vcf.gz > merged_MUC_sorted_dedup_MUC5AC_MUC5B_stats.txt
+## 5054 variants for 63 samples
