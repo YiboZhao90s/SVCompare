02615f76d9e8222dd29617f91efb283e5ae9e79f
Yibo Zhao <yibozhao2297@gmail.com>
2026-01-26 11:09:30 +0000

    Update merge_plender_data.sh

--- merge_plender_data.sh
+++ merge_plender_data.sh
@@ -1,5 +1,5 @@
 # Merge Plender_Data
 module load bcftools/1.16-kjo5veq  
-cd /data/gen1/yz735/SV_explore/GRCh38 
+cd /data/gen1/yz735/mucinSV/plender 
 bcftools concat -a -Oz -o variants_GRCh38_merged_HGSVC.vcf.gz *vcf.gz  
 bcftools index --tbi variants_GRCh38_merged_HGSVC.vcf.gz
